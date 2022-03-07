8f47c865a86aa43fc6a3269a32357af6e88f279c
NosiGuapo <nosicreations@gmail.com>
2022-03-07 17:27:08 +0100

    Updating doctor's profile page

--- lib/screens/Doctors/DoctorPage/components/body.dart
+++ lib/screens/Doctors/DoctorPage/components/body.dart
@@ -2,7 +2,7 @@ import 'package:ap4_gsbmedecins_appli/entities/Doctor.dart';
 // import 'package:ap4_gsbmedecins_appli/services/DoctorService.dart';
 import 'package:flutter/material.dart';
 import 'package:ap4_gsbmedecins_appli/screens/Doctors/DoctorPage/components/background.dart';
-import '../../../../constants.dart';
+// import '../../../../constants.dart';
 
 class Body extends StatelessWidget {
   final Doctor doctor;
@@ -11,16 +11,35 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String spec;
+    if (doctor.spec == null){
+      spec = "Ce médecin ne possède aucune spécialité";
+    } else {
+      spec = doctor.spec!;
+    }
     return Background(
       bg: Scaffold(
         appBar: AppBar(
-          title: Text("Page du médecin"),
-          backgroundColor: primaryColour,
+          title: const Text(
+              "Page du médecin",
+            style: TextStyle(
+              color: Colors.black54,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Roboto',
+              letterSpacing: -0.2
+            ),
+          ),
+          elevation: 0,
+          iconTheme: const IconThemeData(
+            color: Colors.black45
+          ),
+          backgroundColor: Colors.transparent,
         ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              const SizedBox(height: 55),
               const CircleAvatar(
                 backgroundImage: AssetImage("assets/images/profile.png"),
                 radius: 70,
@@ -33,7 +52,24 @@ class Body extends StatelessWidget {
                     fontSize: 18,
                     fontFamily: 'Roboto'),
               ),
-              const SizedBox(height: 65),
+              const SizedBox(height: 35),
+              ListTile(
+                title: const Text("Adresse"),
+                subtitle: Text(doctor.adresse),
+                leading: const Icon(Icons.home),
+              ),
+              ListTile(
+                title: const Text("Numéro de téléphone"),
+                subtitle: Text(doctor.tel),
+                leading: const Icon(Icons.phone),
+              ),
+              ListTile(
+                title: const Text("Spécialité"),
+                subtitle: Text(
+                    spec
+                ),
+                leading: const Icon(Icons.medical_services),
+              ),
               // ElevatedButton(
               //   onPressed: () {
               //     DoctorService.deleteDoctor(doctor.id);
