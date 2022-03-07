5a344202d2604cf6ab5a7feb16bc760c70520cf4
NosiGuapo <nosicreations@gmail.com>
2022-03-07 17:45:38 +0100

    Sending "delete" request instead of "post" one

--- lib/services/DoctorService.dart
+++ lib/services/DoctorService.dart
@@ -21,7 +21,7 @@ class DoctorService{
 
   static Future<bool> deleteDoctor(int id) async {
     final url = Uri.parse('http://10.0.2.2:8080/gsb/medecins/delete/'+id.toString());
-    final response = await http.post(url);
+    final response = await http.delete(url);
 
     if (response.statusCode == 200){
       return true;
