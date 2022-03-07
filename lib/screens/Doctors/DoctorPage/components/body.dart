7f147dea590e5674b17e1c521bbaea0167a436e6
NosiGuapo <nosicreations@gmail.com>
2022-03-07 17:06:14 +0100

    Commenting remove button on doctor page (unused)

--- lib/screens/Doctors/DoctorPage/components/body.dart
+++ lib/screens/Doctors/DoctorPage/components/body.dart
@@ -1,5 +1,5 @@
 import 'package:ap4_gsbmedecins_appli/entities/Doctor.dart';
-import 'package:ap4_gsbmedecins_appli/services/DoctorService.dart';
+// import 'package:ap4_gsbmedecins_appli/services/DoctorService.dart';
 import 'package:flutter/material.dart';
 import 'package:ap4_gsbmedecins_appli/screens/Doctors/DoctorPage/components/background.dart';
 import '../../../../constants.dart';
@@ -34,22 +34,22 @@ class Body extends StatelessWidget {
                     fontFamily: 'Roboto'),
               ),
               const SizedBox(height: 65),
-              ElevatedButton(
-                onPressed: () {
-                  DoctorService.deleteDoctor(doctor.id);
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: failToastColour,
-                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 13),
-                ),
-                child: const Text(
-                  "Supprimer",
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 17
-                  ),
-                ),
-              )
+              // ElevatedButton(
+              //   onPressed: () {
+              //     DoctorService.deleteDoctor(doctor.id);
+              //   },
+              //   style: ElevatedButton.styleFrom(
+              //     primary: failToastColour,
+              //     padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 13),
+              //   ),
+              //   child: const Text(
+              //     "Supprimer",
+              //     style: TextStyle(
+              //       fontFamily: 'Roboto',
+              //       fontSize: 17
+              //     ),
+              //   ),
+              // )
             ],
           ),
         ),
