ce3828ec6136aa0c93f14d355000f07db68e10cd
Alexandre G <77666432+NosiGuapo@users.noreply.github.com>
2022-03-09 11:01:52 +0100

    get doctor by its last name method

--- lib/services/DoctorService.dart
+++ lib/services/DoctorService.dart
@@ -32,4 +32,21 @@ class DoctorService{
       return false;
     }
   }
+
+  static Future<List<Doctor>> searchDoctorsByLastName(String value) async{
+    // final url = Uri.parse('http://10.0.2.2:8080/gsb/medecins/lname/'+value.toString());
+    final url = Uri.parse('http://172.31.1.95:8080/gsb/medecins/lname/'+value.toString());
+    final response = await http.get(url);
+
+    if (response.statusCode == 200){
+      List<dynamic> body = json.decode(utf8.decode(response.bodyBytes));
+      var doctors = body.map((body) {
+        return Doctor.fromJson(body);
+      }).toList();
+      return doctors;
+    } else {
+      print('Une erreur est survenue lors de l\'accès aux données (searchDoctorsByLastName): Erreur '+response.statusCode.toString());
+      return List<Doctor>.empty();
+    }
+  }
 }
