d469c5f82bf8b1930e81ca1718bdf9309a092672
NosiGuapo <nosicreations@gmail.com>
2022-03-13 19:36:45 +0100

    test2

--- lib/screens/Doctors/DoctorList/components/body.dart
+++ lib/screens/Doctors/DoctorList/components/body.dart
@@ -42,7 +42,7 @@ class _BodyState extends State<Body> {
                   onPressed: () {
                     showSearch(context: context, delegate: DoctorSearch());
                   },
-                  icon: const Icon(Icons.search, color: Colors.blue)
+                  icon: const Icon(Icons.search, color: Colors.green)
               ),
             ],
             title: SizedBox(
