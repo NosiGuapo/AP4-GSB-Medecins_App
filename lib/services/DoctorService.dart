bbd7c59b48fb5760a9fd7f64857adde9a3271511
NosiGuapo <nosicreations@gmail.com>
2022-03-18 19:33:32 +0100

    GetDoctorById method

--- lib/services/DoctorService.dart
+++ lib/services/DoctorService.dart
@@ -28,6 +28,20 @@ class DoctorService{
     }
   }
 
+  static Future<Doctor?> getDoctorById(int? id) async {
+    final url = Uri.parse('http://10.0.2.2:8080/gsb/medecins/'+id.toString());
+
+    final response = await http.get(url);
+    if (response.statusCode == 200){
+      Map<String, dynamic> body = json.decode(utf8.decode(response.bodyBytes));
+      var doctor = Doctor.fromJson(body);
+      return doctor;
+    } else {
+      print('Une erreur est survenue lors de l\'accès aux données (getDoctorById): Erreur '+response.statusCode.toString());
+      return null;
+    }
+  }
+
   static Future<bool> deleteDoctor(int id) async {
     final url = Uri.parse('http://10.0.2.2:8080/gsb/medecins/delete/'+id.toString());
     // final url = Uri.parse('http://172.31.1.95:8080/gsb/medecins/delete/'+id.toString());
